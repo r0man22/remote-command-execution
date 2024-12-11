d7779c22d72683a2bb3676fd1fcffb509e39343c
Samcr00t <roman.mammadov22@gmail.com>
2024-12-11 14:53:28 -0500

    add command

--- test/test_client.py
+++ test/test_client.py
@@ -6,12 +6,13 @@ import subprocess
 async def send_command():
     uri = "ws://192.168.1.5:8765"  # SERVER_IP yerine sunucunun IP adresini yazın
     async with websockets.connect(uri) as websocket:
-        command = "find / -type f -perm -04000 -exec basename {} \; 2>/dev/null"  # Desktop dizinini listeleyen komut
-        await websocket.send(command)
-        print("Komut gönderildi, yanıt bekleniyor...")
-        output = await websocket.recv()
-        print("Sunucudan gelen yanıt:")
-        print(output)
+        commands = ["ls ~/Desktop", "whoami"]
+	for command in commands:
+        	await websocket.send(command)
+        	print("Komut gönderildi, yanıt bekleniyor...")
+        	output = await websocket.recv()
+        	print("Sunucudan gelen yanıt:")
+        	print(output)
 
 if __name__ == "__main__":
     asyncio.run(send_command())
